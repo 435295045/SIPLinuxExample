0a374ad08933dbc86643f1c8ea76ea619af8d867
tyang <yangtaotop@126.com>
2024-06-23 19:13:16 -0700

    更新授权文件

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -15,7 +15,7 @@ int main()
             // 3s后测试呼叫
             std::map<std::string, std::string> headers;
             // 这里呼叫多个
-            sip::callIP("10.200.0.1", headers);
+            sip::callIP("10.200.0.5", headers);
             sip::callIP("10.200.0.4", headers);
         }
     }
